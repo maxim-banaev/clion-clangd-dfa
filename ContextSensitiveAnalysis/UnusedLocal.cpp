8c1ef19e575eb7592f8b8983a85bd92f57959cdc
Maxim.Banaev <Maxim.Banaev@jetbrains.com>
2019-10-17 11:01:01 +0300

    update UnusedLocal.cpp

--- ContextSensitiveAnalysis/UnusedLocal.cpp
+++ ContextSensitiveAnalysis/UnusedLocal.cpp
@@ -1,3 +1,6 @@
+#include <string>
+#include <vector>
+
 namespace {
 void test1() {
   int x = 0;
@@ -10,4 +13,23 @@ void test3() {
   int x = 2;
   x = x + 1;
 }
+
+class foo {
+  int x;
+
+public:
+  static void test4(int x) { int y; }
+};
+
+void test5(foo f) { foo f1; }
+
+void test6() { std::string s = "123"; }
+
+void test7(std::string s, std::vector<int> v) {
+  std::string s1;
+  std::vector<int> v1;
+}
+
+template <typename T> void test8(T t) {}
+
 } // namespace
