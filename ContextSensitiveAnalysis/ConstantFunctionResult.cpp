57ae6979230555c0359634c023cd4f85140336b6
Maxim Banaev <Maxim.Banaev@jetbrains.com>
2020-12-17 11:05:16 +0300

    Add some cases for Constant Function Result and Constant Parameter inspections

---
+++ ContextSensitiveAnalysis/ConstantFunctionResult.cpp
@@ -0,0 +1,132 @@
+#include <string>
+#include <unistd.h>
+
+// Global DFA
+namespace {
+    // https://youtrack.jetbrains.com/issue/CPP-23365
+    bool test1() {
+        return true;
+    }
+
+    bool test1_1(int x, bool b = true) {
+        if (x > 0)
+            return b;
+        return true;
+    }
+
+    auto test1_2(int x, bool b = true) -> bool{
+        if (x > 0)
+            return b;
+        return true;
+    }
+
+    // It seems that inspection is working only with int-convertible types
+    std::string test2(int x, std::string s = "") {
+        if (x % 2 == 0)
+            return s;
+        sleep(x);
+        return "";
+    }
+
+    char test3(int x, char c = '3') {
+        if (x % 2 == 0)
+            return c;
+        sleep(x);
+        return '3';
+    }
+
+    auto test4(int x, int b = 0) -> decltype(1){
+        if (x > 0)
+            return b;
+        return 0;
+    }
+
+    // https://youtrack.jetbrains.com/issue/CPP-23367
+    template<int N>
+    int test5(int x, int b = N) {
+        if (x > 0)
+            return b;
+        return N;
+    }
+
+    // https://youtrack.jetbrains.com/issue/CPP-23367
+    template<typename T>
+    T test6(T x, T b = 0) {
+        if (x > 0)
+            return b;
+        return 0;
+    }
+
+    enum class Letter {
+        A,
+        B
+    };
+
+    Letter test7(int x, Letter l = Letter::A) {
+        if (x > 0)
+            return l;
+        return Letter::A;
+    }
+
+    // https://youtrack.jetbrains.com/issue/CPP-23378
+    int test8(int x) {
+        if (random() % 2 == 0)
+            return test8(x);
+        return x;
+    }
+
+    void* test9(int x, void* p = nullptr) {
+        if (x > 0)
+            return p;
+        return nullptr;
+    }
+
+    // https://youtrack.jetbrains.com/issue/CPP-23379
+    int test10(int x, int b = 0) {
+        if (x > 0)
+            return b;
+        return 0;
+    }
+
+
+}
+
+// https://youtrack.jetbrains.com/issue/CPP-23365
+static bool test15() {
+    return true;
+}
+
+
+class X {
+    static int number;
+
+    static int getNUmber(int offset) {
+        return number - offset;
+    }
+
+public:
+    void test16() {
+        int tmp = getNUmber(0);
+    }
+};
+
+int X::number = 0;
+
+void checkGlobalDFA() {
+    test1();
+    test1_1(1);
+    test1_2(1);
+    test2(2);
+    test3(3);
+    test4(4);
+    test5<0>(5);
+    test6<int>(6);
+    test7(7);
+    test8(8);
+    test9(9);
+
+    std::function<int(int, int)> f_test10 = test10;
+    f_test10(10, 0);
+
+    test15();
+}
---
+++ ContextSensitiveAnalysis/ConstantParameter.cpp
@@ -0,0 +1,108 @@
+
+// Constant parameter is working only with int-convertible types
+// It's not working with other types even int() operator is defined
+// https://youtrack.jetbrains.com/issue/CPP-23268
+
+
+#include <climits>
+#include <string>
+#include <vector>
+
+void static test1(int a) {
+
+}
+
+void static test2(const int &a) { // look like is not working also
+
+}
+
+void static test3(const std::string &s) {
+
+}
+
+void static test4(const int p) {}
+
+void static test5(bool flag) {}
+
+template<typename T>
+void static test6(T t) {}
+
+enum Color {
+    Blue = 0
+};
+enum class Letter {
+    A = 0
+};
+
+void static test7(Color c, Letter l) {}
+
+
+
+namespace {
+    void test11(int a) {}
+
+    void test11_1(int a = 0) {}
+
+    void test11_2(int a, int b) {}
+
+    void test11_3(char c, int a) {}
+
+
+    void test12() {}
+
+    template<typename T>
+    void test15(T t) {}
+
+    template<typename T, typename U>
+    void test16(T t, U u = '.'){}
+
+    void test17(const int numbers...) {}
+}
+
+
+class X {
+    static int zero;
+    bool flag;
+
+    void XTest1(int p) {}
+
+    static int XTest2(unsigned p) {
+        return p;
+    }
+
+public:
+    void chechDFA() {
+        XTest1(1);
+        auto ret = flag ? XTest2(1) : 0;
+
+
+    }
+};
+
+struct point {
+    int x;
+    int y;
+};
+
+void checkGlobalDFA() {
+    test1(1);
+    test2(2);
+    test3("3");
+    test4(4);
+    test5(true);
+    test6(1);
+    test7(Blue, Letter::A);
+
+    test11(INT_MAX);
+    test11_1(0);
+    test11_2(0, 1);
+    test11_3('_', 1);
+
+    test15<int>(0);
+    test16<int, char>(1 );
+    test17(1, 2);
+}
+
+
+
+
