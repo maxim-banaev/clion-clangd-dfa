20fb9ae842ddbbca698e32661c45c72b937361df
Maxim Banaev <Maxim.Banaev@jetbrains.com>
2020-12-22 13:41:11 +0300

    Add some cases for Unreachable Call of Functions

--- ContextSensitiveAnalysis/UnreachableCall.cpp
+++ ContextSensitiveAnalysis/UnreachableCall.cpp
@@ -51,7 +51,7 @@ namespace {
 
     std::string test7_function() { return ""; }
     void test7() {
-        if (flag) test6_function();
+        if (flag) test7_function();
     }
 
     template<typename T>
@@ -68,11 +68,70 @@ class Test9 {
     static void test9_function() {}
 
 public:
-    void test9() {
-        bool a = false;
-        if (a) test9_function();
-    }
+    void test9() { if (flag) test9_function();}
 };
+
+static void test10_function() {}
+#define _FUNC_NAME test10_function
+void test10() { if (flag) _FUNC_NAME(); }
+
+static void test11_function();
+static void test11_function() {}
+void test11() { if (flag) test11_function();}
+
+static void test12_function() {}
+void do_nothing() {}
+void test12() { flag ? test12_function() : do_nothing();}
+
+static void test13_function() {}
+// https://youtrack.jetbrains.com/issue/CPP-23440
+void test13() {
+    try {
+
+    } catch (...) {
+        test13_function();
+    }
+}
+
+static void test14_function() {}
+// https://youtrack.jetbrains.com/issue/CPP-23441
+void test14() { return; test14_function();}
+
+static void test15_function() {}
+// https://youtrack.jetbrains.com/issue/CPP-23442
+void test15() { while (true) {} test15_function();}
+
+static void test16_function() {}
+// https://youtrack.jetbrains.com/issue/CPP-23443
+void test16() {
+    goto test16_label;
+    test16_function();
+    test16_label:
+    return;
+}
+
+static bool test17_function() { return true;}
+void test17() {
+    if ( !flag || test17_function()) { do_nothing(); }
+}
+
+static bool test18_function() { return true;}
+void test18() {
+    if ( !flag) { do_nothing();
+    } else if (test18_function()) { }
+}
+
+static bool test19_function() { return true;}
+void test19() {
+    if ( flag && test19_function()) { do_nothing(); }
+}
+
+static bool test20_function() { return true;}
+void test20(int a) {
+    for (int i = 0; true || test20_function(); i++) {
+
+    }
+}
 
 void chekGlobalDFA() {
     ::test6();
