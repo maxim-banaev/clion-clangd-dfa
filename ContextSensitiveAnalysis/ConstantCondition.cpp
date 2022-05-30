61e2d2e01d3a1dc4a7372a9f782755b0bb4eb95a
Maxim Banaev <Maxim.Banaev@jetbrains.com>
2022-05-30 14:03:30 +0300

    add test case for CPP-29088

--- ContextSensitiveAnalysis/ConstantCondition.cpp
+++ ContextSensitiveAnalysis/ConstantCondition.cpp
@@ -1,8 +1,7 @@
-// summary: 50 warnings
+// summary: 52 warnings
 
 #include <iostream>
 #include <type_traits>
-
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wconstant-evaluated"
@@ -11,432 +10,465 @@
 #pragma ide diagnostic ignored "ConstantFunctionResult"
 // Local DFA
 namespace constant_condition {
-    void test() {
-        int a = 1;
-        if (a == 1) { // warn here
-        }
+void test() {
+  int a = 1;
+  if (a == 1) { // warn here
+  }
 
-        // check the suppressing
+  // check the suppressing
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "ConstantConditionsOC"
-        if (a != 0) {
-        }
+  if (a != 0) {
+  }
 #pragma clang diagnostic pop
-    }
+}
 
 #define A(x) x == 1
 
-    void test1() {
-        int a = 1;
-        if (A(a)) { // https://youtrack.jetbrains.com/issue/CPP-17805
-        }
-    }
+void test1() {
+  int a = 1;
+  if (A(a)) { // https://youtrack.jetbrains.com/issue/CPP-17805
+  }
+}
 
-    void test2() {
-        int a = 0;
-        int b = 0;
+void test2() {
+  int a = 0;
+  int b = 0;
 
-        int *a_ptr = &a;
-        *a_ptr = 1; // Modifies a, now a=1, b=0.
+  int *a_ptr = &a;
+  *a_ptr = 1; // Modifies a, now a=1, b=0.
 
-        if (a != b) { // shouldn't be there
-        }
-    }
+  if (a != b) { // shouldn't be there
+  }
+}
 
-    void test3() {
-        int first = 0;
-        int last = 0;
+void test3() {
+  int first = 0;
+  int last = 0;
 
-        std::cin >> first >> last;
+  std::cin >> first >> last;
 
-        if (first == last) { // shouldn't be there
-        }
-    }
+  if (first == last) { // shouldn't be there
+  }
+}
 
-    template<typename T>
-    void test4();
+template <typename T> void test4();
 
-    template<>
-    void test4<int>() {
-        int t1 = 0;
-        int t2 = 0;
-        if (t1 == t2) { // warn here
-        }
-    }
+template <> void test4<int>() {
+  int t1 = 0;
+  int t2 = 0;
+  if (t1 == t2) { // warn here
+  }
+}
 
-    void test5() {
-        int *i = nullptr;
-        if (i == nullptr) {} // warn here
+void test5() {
+  int *i = nullptr;
+  if (i == nullptr) {
+  } // warn here
 
-        std::string *s = nullptr;
-        if (s == nullptr) {} // warn here
-    }
+  std::string *s = nullptr;
+  if (s == nullptr) {
+  } // warn here
+}
 
-    typedef int X;
+typedef int X;
 
-    void test6() {
-        X i = 1;
-        if (i == 1) {} // warn here
-    }
+void test6() {
+  X i = 1;
+  if (i == 1) {
+  } // warn here
+}
 
 #define X 1
 #define Y y
 
-    void test7() {
-        int x = 1;
-        int y = 1;
+void test7() {
+  int x = 1;
+  int y = 1;
 
-        if (x == X) {} // warn here
-        if (X == x) {} // https://youtrack.jetbrains.com/issue/CPP-17805
-        if (Y == 1) {} // https://youtrack.jetbrains.com/issue/CPP-17805
-        if (1 == Y) {} // warn here
-        if (x == Y) {} // warn here
-        if (Y == x) {} // https://youtrack.jetbrains.com/issue/CPP-17805
-        if (Y == X) {} // https://youtrack.jetbrains.com/issue/CPP-17805
-        if (X == Y) {} // https://youtrack.jetbrains.com/issue/CPP-17805
-    }
+  if (x == X) {
+  } // warn here
+  if (X == x) {
+  } // https://youtrack.jetbrains.com/issue/CPP-17805
+  if (Y == 1) {
+  } // https://youtrack.jetbrains.com/issue/CPP-17805
+  if (1 == Y) {
+  } // warn here
+  if (x == Y) {
+  } // warn here
+  if (Y == x) {
+  } // https://youtrack.jetbrains.com/issue/CPP-17805
+  if (Y == X) {
+  } // https://youtrack.jetbrains.com/issue/CPP-17805
+  if (X == Y) {
+  } // https://youtrack.jetbrains.com/issue/CPP-17805
+}
 
-    void test8() {
-        float f = 0.5;
-        if (f == 0.5) {} // warn here
-    }
+void test8() {
+  float f = 0.5;
+  if (f == 0.5) {
+  } // warn here
+}
 
-    [[maybe_unused]] void test8_1() {
-      float f = 0.5f;
-      if (f == 0.5f) {} // warn here
-    }
+[[maybe_unused]] void test8_1() {
+  float f = 0.5f;
+  if (f == 0.5f) {
+  } // warn here
+}
 
-    bool test9(int p1, [[maybe_unused]] int p2) {
-        if (p1 == 1 || p1 == 2)
-            return p1 == 1 || p1 == 2; // warn here
-        return false;
-    }
+bool test9(int p1, [[maybe_unused]] int p2) {
+  if (p1 == 1 || p1 == 2)
+    return p1 == 1 || p1 == 2; // warn here
+  return false;
+}
 
-    void fn(bool cond);
-    void test10() {
-        int x = 1;
-        fn(x == 1); // warn here
-    }
+void fn(bool cond);
+void test10() {
+  int x = 1;
+  fn(x == 1); // warn here
+}
 
-    void test11() {
-        int x = 1;
+void test11() {
+  int x = 1;
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "UnusedValue"
-        [[maybe_unused]] bool a = x == 1; // warn here
+  [[maybe_unused]] bool a = x == 1; // warn here
 #pragma clang diagnostic pop
+}
+
+class [[maybe_unused]] test12 {
+  int x;
+
+public:
+  [[maybe_unused]] void func() {
+    this->x = 1;
+    if (this->x == 1) {
+    } // warn here
+  }
+};
+
+void test13() {
+  int a = 1;
+  [[maybe_unused]] auto cond = a == 1; // warn here
+}
+
+void fn14([[maybe_unused]] bool flag = false) {}
+void test14() {
+  int a = 1;
+  fn14(a == 1); // warn here
+}
+
+int fn15() { return 0; }
+
+void test15() {
+  int a = 1;
+  if (a == fn15()) {
+  } // warn here
+}
+
+[[maybe_unused]] void test16(bool flag) {
+  int x = 1;
+  [[maybe_unused]] bool a = flag ? x == 1 : x == 2; // warn here twice
+}
+
+void test17() {
+  if (std::is_constant_evaluated()) {
+  } // warn here
+}
+
+[[maybe_unused]] void test18() {
+  [[maybe_unused]] const int n =
+      std::is_constant_evaluated() ? 13 : 17; // warn here
+}
+
+template <int x> struct C {};
+
+template <> struct C<true> {
+  static const int x = 1;
+};
+
+template <> struct C<false> {
+  static const int x = 0;
+};
+
+[[maybe_unused]] constexpr void test19(const int x) {
+  // Type checker correctly evaluates std::is_constant_evaluated() == true here
+  if (C<std::is_constant_evaluated()>::x) {
+  } // warn here
+}
+
+[[maybe_unused]] void test20() {
+  int a = 1;
+  if (a < 2) { // warn here
+  }
+}
+
+[[maybe_unused]] void CPP_28958() {
+  bool state = false;
+  for (int i = 0; i < 10; ++i) {
+    if (state) { // shouldn't warn here
+      std::cout << "true\n";
     }
+    state = !state;
+  }
+}
 
-    class [[maybe_unused]] test12 {
-        int x;
-    public:
-        [[maybe_unused]] void func() {
-            this->x = 1;
-            if (this->x == 1) {} // warn here
-        }
-    };
+static int verbose = 1;
+static void *ptr = &verbose;
+void CPP_29088() {
+  if (verbose) // shouldn't warn here
+    std::cout << "unknown \n";
+}
 
-    void test13() {
-        int a = 1;
-        [[maybe_unused]] auto cond = a == 1; // warn here
-    }
-
-    void fn14([[maybe_unused]] bool flag = false) {}
-    void test14() {
-        int a = 1;
-        fn14( a == 1); // warn here
-    }
-
-    int fn15() {
-        return 0;
-    }
-
-    void test15() {
-        int a = 1;
-        if(a == fn15()) {} // warn here
-    }
-
-    [[maybe_unused]] void test16(bool flag) {
-        int x = 1;
-        [[maybe_unused]] bool a = flag ? x == 1 : x == 2; // warn here twice
-    }
-
-    void test17() {
-        if(std::is_constant_evaluated()) {} // warn here
-    }
-
-    [[maybe_unused]] void test18() {
-        [[maybe_unused]] const int n = std::is_constant_evaluated() ? 13 : 17; // warn here
-    }
-
-    template <int x>
-    struct C {};
-
-    template<>
-    struct C<true> {
-        static const int x = 1;
-    };
-
-    template<>
-    struct C<false> {
-        static const int x = 0;
-    };
-
-    [[maybe_unused]] constexpr void test19(const int x) {
-        // Type checker correctly evaluates std::is_constant_evaluated() == true here
-        if (C<std::is_constant_evaluated()>::x) {} // warn here
-    }
-
-    [[maybe_unused]] void CPP_28958() {
-      bool state = false;
-      for (int i = 0; i < 10; ++i) {
-        if (state) { // shouldn't warn here
-          std::cout << "true\n";
-        }
-        state = !state;
-      }
-    }
-
-    [[maybe_unused]] void test20() {
-      int a = 1;
-      if (a < 2) { // warn here
-      }
-    }
-
-
-} // namespace
-
+} // namespace constant_condition
 
 // Global DFA
 namespace {
-    void test() {
-        int a = 1;
-        if (a == 1) { // warn here
-        }
+void test() {
+  int a = 1;
+  if (a == 1) { // warn here
+  }
 
-        // check the suppressing
+  // check the suppressing
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "ConstantConditionsOC"
-        if (a != 0) {
-        }
+  if (a != 0) {
+  }
 #pragma clang diagnostic pop
-    }
+}
 
 #define A(x) x == 1
 
-    void test1() {
-        int a = 1;
-        if (A(a)) { // https://youtrack.jetbrains.com/issue/CPP-17805
-        }
-    }
+void test1() {
+  int a = 1;
+  if (A(a)) { // https://youtrack.jetbrains.com/issue/CPP-17805
+  }
+}
 
-    void test2() {
-        int a = 0;
-        int b = 0;
+void test2() {
+  int a = 0;
+  int b = 0;
 
-        int *a_ptr = &a;
-        *a_ptr = 1; // Modifies a, now a=1, b=0.
+  int *a_ptr = &a;
+  *a_ptr = 1; // Modifies a, now a=1, b=0.
 
-        if (a != b) { // shouldn't be there
-        }
-    }
+  if (a != b) { // shouldn't be there
+  }
+}
 
-    void test3() {
-        int first = 0;
-        int last = 0;
+void test3() {
+  int first = 0;
+  int last = 0;
 
-        std::cin >> first >> last;
+  std::cin >> first >> last;
 
-        if (first == last) { // shouldn't be there
-        }
-    }
+  if (first == last) { // shouldn't be there
+  }
+}
 
-    template<typename T>
-    void test4();
+template <typename T> void test4();
 
-    template<>
-    void test4<int>() {
-        int t1 = 0;
-        int t2 = 0;
-        if (t1 == t2) { // warn here
-        }
-    }
+template <> void test4<int>() {
+  int t1 = 0;
+  int t2 = 0;
+  if (t1 == t2) { // warn here
+  }
+}
 
-    void test5() {
-        int *i = nullptr;
-        if (i == nullptr) {} // warn here
+void test5() {
+  int *i = nullptr;
+  if (i == nullptr) {
+  } // warn here
 
-        std::string *s = nullptr;
-        if (s == nullptr) {} // warn here
-    }
+  std::string *s = nullptr;
+  if (s == nullptr) {
+  } // warn here
+}
 
-    typedef int X1;
+typedef int X1;
 
-    void test6() {
-        X1 i = 1;
-        if (i == 1) {} // warn here
-    }
+void test6() {
+  X1 i = 1;
+  if (i == 1) {
+  } // warn here
+}
 
 #define X 1
 #define Y y
 
-    void test7() {
-        int x = 1;
-        int y = 1;
+void test7() {
+  int x = 1;
+  int y = 1;
 
-        if (x == X) {} // warn here
-        if (X == x) {} // https://youtrack.jetbrains.com/issue/CPP-17805
-        if (Y == 1) {} // https://youtrack.jetbrains.com/issue/CPP-17805
-        if (1 == Y) {} // warn here
-        if (x == Y) {} // warn here
-        if (Y == x) {} // https://youtrack.jetbrains.com/issue/CPP-17805
-        if (Y == X) {} // https://youtrack.jetbrains.com/issue/CPP-17805
-        if (X == Y) {} // https://youtrack.jetbrains.com/issue/CPP-17805
-    }
+  if (x == X) {
+  } // warn here
+  if (X == x) {
+  } // https://youtrack.jetbrains.com/issue/CPP-17805
+  if (Y == 1) {
+  } // https://youtrack.jetbrains.com/issue/CPP-17805
+  if (1 == Y) {
+  } // warn here
+  if (x == Y) {
+  } // warn here
+  if (Y == x) {
+  } // https://youtrack.jetbrains.com/issue/CPP-17805
+  if (Y == X) {
+  } // https://youtrack.jetbrains.com/issue/CPP-17805
+  if (X == Y) {
+  } // https://youtrack.jetbrains.com/issue/CPP-17805
+}
 
+int getZero() {
+  int a = 0;
+  return a;
+}
 
-    int getZero() {
-        int a = 0;
-        return a;
-    }
+int getOne() {
+  int a = 1;
+  return a;
+}
 
-    int getOne() {
-        int a = 1;
-        return a;
-    }
+void test8() {
+  if (getZero() == 0) {
+  } // warn here
+  if (getZero()) {
+  } // warn here
+  if (!getZero()) {
+  } // warn here
+  if (getZero(
+          // foo
+          )) {
+  } // warn here
 
-    void test8() {
-        if (getZero() == 0) {} // warn here
-        if (getZero()) {} // warn here
-        if (!getZero()) {} // warn here
-        if (getZero(
-                // foo
-                )) {} // warn here
+  if (false) { // warn here
+    asm("nop");
+  } else if (getZero()) {
+  } // warn here
+}
 
-        if (false) { // warn here
-            asm("nop");
-        } else if (getZero()) {} // warn here
-    }
+void test9() {
+  while (getZero()) {
+  } // warn here
+}
 
-    void test9() {
-        while (getZero()) {} // warn here
-    }
+int test10() {
+  return 0;
+  if (getZero() == 0) {
+  } // shouldn't warn here
+}
 
-    int test10() {
-        return 0;
-        if (getZero() == 0) {} // shouldn't warn here
-    }
+void test11() {
+  if (getZero() == 0) {
+  } // warn here
+  if (1 == getOne()) {
+  } // warn here
+  if (getZero() == getOne()) {
+  }
+}
 
-    void test11() {
-        if (getZero() == 0) {} // warn here
-        if (1 == getOne()) {} // warn here
-        if (getZero() == getOne()) { }
-    }
+bool test12(int p1, [[maybe_unused]] int p2) {
+  if (p1 == 1 || p1 == 2)      // warn here
+    return p1 == 1 || p1 == 2; // warn here
+  return false;
+}
 
-    bool test12(int p1, [[maybe_unused]] int p2) {
-        if (p1 == 1 || p1 == 2) // warn here
-            return p1 == 1 || p1 == 2; // warn here
-        return false;
-    }
+void fn([[maybe_unused]] bool cond) {}
+void test13() {
+  int x = 1;
+  fn(false);
+  fn(x == 1); // warn here
+}
 
-    void fn([[maybe_unused]] bool cond) {}
-    void test13() {
-        int x = 1;
-        fn(false);
-        fn(x == 1); // warn here
-    }
-
-    void test14() {
-        int x = 1;
+void test14() {
+  int x = 1;
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "UnusedValue"
-        [[maybe_unused]] auto b = x == 1; // warn here
+  [[maybe_unused]] auto b = x == 1; // warn here
 #pragma clang diagnostic pop
-    }
+}
 
-    void test15() {
-        int a = 1;
-        [[maybe_unused]] auto cond = a == 1; // warn here
-    }
+void test15() {
+  int a = 1;
+  [[maybe_unused]] auto cond = a == 1; // warn here
+}
 
-    void fn16([[maybe_unused]] bool flag = false) {}
-    void test16() {
-        int a = 1;
-        fn16();
-        fn16( a == 1); // warn here
-    }
+void fn16([[maybe_unused]] bool flag = false) {}
+void test16() {
+  int a = 1;
+  fn16();
+  fn16(a == 1); // warn here
+}
 
-    int fn17() {
-        return 0;
-    }
+int fn17() { return 0; }
 
-    void test17() {
-        int a = 1;
-        if(a == fn17()) {} // warn here
-    }
+void test17() {
+  int a = 1;
+  if (a == fn17()) {
+  } // warn here
+}
 
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "ConstantParameter"
-    void test18(float f = 0.f) {
-        [[maybe_unused]] auto cond = f == 0.f; // warn here
-    }
+void test18(float f = 0.f) {
+  [[maybe_unused]] auto cond = f == 0.f; // warn here
+}
 #pragma clang diagnostic pop
 
-    void test19() {
-        if(std::is_constant_evaluated()) {} // warn here
-    }
+void test19() {
+  if (std::is_constant_evaluated()) {
+  } // warn here
+}
 
-    constexpr int test20([[maybe_unused]] bool flag) {
-        if(std::is_constant_evaluated()) { // shouldn't warn here
-            return 1;
-        } else {
-            return 0;
-        }
-    }
+constexpr int test20([[maybe_unused]] bool flag) {
+  if (std::is_constant_evaluated()) { // shouldn't warn here
+    return 1;
+  } else {
+    return 0;
+  }
+}
 
-    void CPP_28958() {
-      bool state = false;
-      for (int i = 0; i < 10; ++i) {
-        if (state) { // shouldn't warn here
-          std::cout << "true\n";
-        }
-        state = !state;
-      }
-    }
+[[maybe_unused]] void test21() {
+  int a = 1;
+  if (a < 2) { // warn here
+  }
+}
 
-    [[maybe_unused]] void test21() {
-      int a = 1;
-      if (a < 2) { // warn here
-      }
+void CPP_28958() {
+  bool state = false;
+  for (int i = 0; i < 10; ++i) {
+    if (state) { // shouldn't warn here
+      std::cout << "true\n";
     }
+    state = !state;
+  }
+}
 } // namespace
 
-
 void checkGlobalDFA() {
-    ::test();
-    ::test1();
-    ::test2();
-    ::test3();
-    ::test4<int>();
-    ::test5();
-    ::test6();
-    ::test7();
-    ::test8();
-    ::test9();
-    ::test10();
-    ::test11();
-    ::test12(1,2);
-    ::test12(2,1);
-    ::test13();
-    ::test14();
-    ::test15();
-    ::test16();
-    ::test17();
-    ::test18();
-    ::test19();
-    [[maybe_unused]] constexpr int f = ::test20(true);
+  ::test();
+  ::test1();
+  ::test2();
+  ::test3();
+  ::test4<int>();
+  ::test5();
+  ::test6();
+  ::test7();
+  ::test8();
+  ::test9();
+  ::test10();
+  ::test11();
+  ::test12(1, 2);
+  ::test12(2, 1);
+  ::test13();
+  ::test14();
+  ::test15();
+  ::test16();
+  ::test17();
+  ::test18();
+  ::test19();
+  [[maybe_unused]] constexpr int f = ::test20(true);
+  ::test21();
 
-    ::CPP_28958();
-
-    ::test21();
+  ::CPP_28958();
 }
 #pragma clang diagnostic pop
