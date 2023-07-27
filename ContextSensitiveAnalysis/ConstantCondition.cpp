4c14821c9ed73e0bd36a3157f4796341e25e6a79
Maxim.Banaev <Maxim.Banaev@jetbrains.com>
2023-07-27 15:19:18 +0200

    Add golden result

--- ContextSensitiveAnalysis/ConstantCondition.cpp
+++ ContextSensitiveAnalysis/ConstantCondition.cpp
@@ -30,7 +30,7 @@ void test() {
 #pragma clang diagnostic pop
 }
 
-#define A(index) x == 1
+#define A(index) a == 1
 
 void test1() {
   int a = 1;
@@ -291,7 +291,7 @@ void test() {
 #pragma clang diagnostic pop
 }
 
-#define A(index) x == 1
+#define A(index) a == 1
 
 void test1() {
   int a = 1;
